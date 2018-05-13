6ae9253a5ebcc8c340093a6a61d7a1a809abe008
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2018-05-13 11:34:24 +0300

    Add operational leaflet widget
    Loading time is a bit too much, may need to simplify the municipal polygon data.

--- shiny/app.R
+++ shiny/app.R
@@ -7,42 +7,95 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
+library(shinycssloaders)
+library(shinydashboard)
+library(leaflet)
+library(RColorBrewer)
+library(sf)
+
+fin_muni <- sf::read_sf("../data/kuntajako_2017_maa_alueet.geojson") %>%
+  dplyr::mutate(check = NAMEFIN == "Jyväskylä") %>% 
+  dplyr::select(check)
+
+check_colors <-c("TRUE" = "#A3A3A3", "FALSE" = "#595490")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+ui <- dashboardPage(
+  dashboardHeader(
+    title = "Magpie!"
+  ),
+  dashboardSidebar(
+    disable = TRUE
+  ),
+  dashboardBody(
+  fluidRow(
+    column(width = 3,
+           box(width = NULL, status = "warning",
+               uiOutput("routeSelect"),
+               checkboxGroupInput("directions", "Show",
+                                  choices = c(
+                                    Northbound = 4,
+                                    Southbound = 1,
+                                    Eastbound = 2,
+                                    Westbound = 3
+                                  ),
+                                  selected = c(1, 2, 3, 4)
+               ),
+               p(
+                 class = "text-muted",
+                 paste("Note: a route number can have several different trips, each",
+                       "with a different path. Only the most commonly-used path will",
+                       "be displayed on the map."
+                 )
+               ),
+               actionButton("zoomButton", "Zoom to fit buses")
+           ),
+           box(width = NULL, status = "warning",
+               selectInput("interval", "Refresh interval",
+                           choices = c(
+                             "30 seconds" = 30,
+                             "1 minute" = 60,
+                             "2 minutes" = 120,
+                             "5 minutes" = 300,
+                             "10 minutes" = 600
+                           ),
+                           selected = "60"
+               ),
+               uiOutput("timeSinceLastUpdate"),
+               actionButton("refresh", "Refresh now"),
+               p(class = "text-muted",
+                 br(),
+                 "Source data updates every 30 seconds."
+               )
+           )
       ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
+      column(width = 9,
+           box(width = NULL, solidHeader = TRUE,
+               withSpinner(leafletOutput("munimap", height = 800), type = 8)
+           )
       )
-   )
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  output$munimap <- renderLeaflet({
+    
+    check_pal <- colorFactor(check_colors, names(check_colors))
+    
+    map <- leaflet(fin_muni) %>%
+      addProviderTiles(providers$Hydda.Full) %>% 
+      #addTiles('http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png') %>%
+      addPolygons(
+        fillColor = ~check_pal(check),
+        weight = 0.5,
+        opacity = 0.8,
+        color = "white",
+        #dashArray = "3",
+        fillOpacity = 0.8)
+  })
 }
 
 # Run the application 
